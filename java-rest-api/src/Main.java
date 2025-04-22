411861c010acec829513e701b62d673902f00884
Pratyushrai2004 <raipratyush39@gmail.com>
2025-04-22 15:22:02 +0530

    commit

---
+++ java-rest-api/src/Main.java
@@ -0,0 +1,29 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("Server running on http://localhost:8080");
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) {
+            try {
+                String response = "Hello from bare-bones Java server!";
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
